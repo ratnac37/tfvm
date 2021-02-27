e2dc709dd1dce386564c3ab1cd1c81cea3904a09
prasad <prasad@gmail.com>
2021-02-27 18:20:39 +0530

    updated main file

---
+++ main.tf
@@ -0,0 +1,93 @@
+resource "azurerm_resource_group" "main" {
+  name     = "devops-RG"
+  location = "centralus"
+}
+resource "azurerm_virtual_network" "main" {
+  name                = "devopsvnet"
+  address_space       = ["14.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "devopssubnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "14.0.0.0/24"
+}
+resource "azurerm_public_ip" "main" {
+  name                = "devopspip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "Production"
+  }
+}
+resource "azurerm_network_interface" "main" {
+  name                = "devopsnic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "ipname"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+resource "azurerm_network_security_group" "main" {
+  name                = "nsgrule"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  security_rule {
+    access                     = "Allow"
+    direction                  = "Inbound"
+    name                       = "RDP"
+    priority                   = 100
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    source_address_prefix      = "*"
+    destination_port_range     = "3389"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = "devopsvm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "adminuser"
+  admin_password      = "Azure@123456"
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
+
+
+
+
+
+
+
+
+
---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+  version         = "=2.46.0"
+  subscription_id = "a38479ae-7402-45e0-8948-3b410d4e4c97"
+  client_id       = "a21cc4f6-0085-4359-a3d9-8930ad0dea9e"
+  client_secret   = "Oo5T7lgtUz4m~~RpD607CbDdo.isE-.0yy"
+  tenant_id       = "f8282601-8313-4145-9e37-df17476ec548"
+}
